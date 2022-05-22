cc81d814dd616fabd4ed14b82c73718cfca079c4
Polymicro <105283617+PolymicroDev@users.noreply.github.com>
2022-05-22 16:27:08 +0200

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,97 +1,39 @@
 import random
+import webbrowser
 
+def num_range(x,y):
+    print(f"I have thought of a number between {x} and {y}")
+    return random.randint(x,y)   
+computer_number = num_range(1,15)
 
-range_num = int(input("What number is the maximum I can think of? "))
-x = range_num
-comp_number = random.randint(1,x)
+def difficulty(dif):
+    match dif:
+        case "easy":
+            turn = 15
+            return turn
+        case "medium":
+            turn = 10
+            return turn
+        case "hard":
+            turn = 5
+            return turn    
+        case "impossible":
+            turn = 1
+            return turn
 
-gamemode = int(input("Press [1] to give your own turns or press [2] for game difficulty selection: "))
-if gamemode == 1:
-    turns = int(input("How many tries do you want to get? "))
-    if turns <= 5:
-        print("You are going crazy! That is gonna be hard man!")
-        while turns > 0:
-            guess = int(input(f"I thought of a number. Make a guess, you have {turns} turns left: "))
-            if guess == comp_number:
-                print(f"Yeah! The correct number is truly {comp_number}!")
-                break
-            elif guess > comp_number:
-                print("Sorry, the number is smaller than that")
-                turns -= 1
-            elif guess < comp_number:
-                print("Sorry, the number is bigger than that than that")
-                turns -= 1
-    elif turns > 5 and turns <= 10:
-        print("All right, let's start")
-        while turns > 0:
-            guess = int(input(f"I thought of a number. Make a guess, you have {turns} turns left: "))
-            if guess == comp_number:
-                print(f"Yeah! The correct number is truly {comp_number}!")
-                break
-            elif guess > comp_number:
-                print("Sorry, the number is smaller than that")
-                turns -= 1
-            elif guess < comp_number:
-                print("Sorry, the number is bigger than that than that")
-                turns -= 1
-    elif turns > 10:
-        print("Isn't that too easy for you?")
-        while turns > 0:
-            guess = int(input(f"I thought of a number. Make a guess, you have {turns} turns left: "))
-            if guess == comp_number:
-                print(f"Yeah! The correct number is truly {comp_number}!")
-                break
-            elif guess > comp_number:
-                print("Sorry, the number is smaller than that")
-                turns -= 1
-            elif guess < comp_number:
-                print("Sorry, the number is bigger than that than that")
-                turns -= 1
-
-elif gamemode == 2:
-    difficulty = int(input("Choose difficulty: Easy [1], Medium [2], Hard [3]: "))
-    if difficulty == 1:
-        turns = 15
-        while turns > 0:
-            guess = int(input(f"I thought of a number. Make a guess, you have {turns} turns left: "))
-            if guess == comp_number:
-                print(f"Yeah! The correct number is truly {comp_number}!")
-                break
-            elif guess > comp_number:
-                print("Sorry, the number is smaller than that")
-                turns -= 1
-            elif guess < comp_number:
-                print("Sorry, the number is bigger than that than that")
-                turns -= 1
-    elif difficulty == 2:
-        turns = 10
-        while turns > 0:
-            guess = int(input(f"I thought of a number. Make a guess, you have {turns} turns left: "))
-            if guess == comp_number:
-                print(f"Yeah! The correct number is truly {comp_number}!")
-                break
-            elif guess > comp_number:
-                print("Sorry, the number is smaller than that")
-                turns -= 1
-            elif guess < comp_number:
-                print("Sorry, the number is bigger than that than that")
-                turns -= 1
-    elif difficulty == 3:
-        turns = 5
-        while turns > 0:
-            guess = int(input(f"I thought of a number. Make a guess, you have {turns} turns left: "))
-            if guess == comp_number:
-                print(f"Yeah! The correct number is truly {comp_number}!")
-                break
-            elif guess > comp_number:
-                print("Sorry, the number is smaller than that")
-                turns -= 1
-            elif guess < comp_number:
-                print("Sorry, the number is bigger than that than that")
-                turns -= 1
-
-
-
-
-
-
+turn = difficulty("hard")
+while turn != 0:
+    guess = int(input(f"Make a guess, you have {turn} left: "))
+    if guess > computer_number:
+        print("Too high, sorry, try again")
+        turn -= 1
+    elif guess < computer_number:
+        print("Too low, sorry, try again")
+        turn -= 1
+    elif turn == 0:
+        print("You lose...") 
+        webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    else:
+        print(f"Yeah, the correct number is {computer_number}!")
+        webbrowser.open("https://media3.giphy.com/media/9xt1MUZqkneFiWrAAD/giphy.gif?cid=ecf05e47kt1op7lofoxmysqqgxqgjwea64mi0bzpiogxugmn&rid=giphy.gif&ct=g")
+        
