d8f4662f919d5930be69f1a6f10c6e10aec6d7c6
PolyMicro <105283617+PolymicroDev@users.noreply.github.com>
2022-06-06 14:44:11 +0200

    Update quadratic equation solver.py

--- quadratic equation solver.py
+++ quadratic equation solver.py
@@ -1,9 +1,9 @@
 import math
 
 def quadratic_equation_solver(a,b,c):
-    result1 = float((-b + math.sqrt(b ** 2 - 4 * a * c)) / 2*a)
-    result2 = float((-b - math.sqrt(b ** 2 - 4 * a * c)) / 2*a)
-    print(f"x1 is: {result1} x2 is: {result2}")
+    root1 = float((-b + math.sqrt(b ** 2 - 4 * a * c)) / 2*a)
+    root2 = float((-b - math.sqrt(b ** 2 - 4 * a * c)) / 2*a)
+    print(f"x1 is: {root1} x2 is: {root2}")
     
 
 quadratic_equation_solver(1, -5, 2)
